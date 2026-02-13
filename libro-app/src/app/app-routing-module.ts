7ee87cd7112c33063071a0dfa724e4ade4c7b63d
1724158488 <xtorres@tecnologicoismac.edu.ec>
2026-02-12 20:56:02 -0500

    libro html

--- libro-app/src/app/app-routing-module.ts
+++ libro-app/src/app/app-routing-module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClienteComponent } from './components/cliente/cliente';
 import { AutorComponent } from './components/autor/autor';
 import { CategoriaComponent } from './components/categoria/categoria';
+import { LibroComponent } from './components/libro/libro';
 
 const routes: Routes = [
   { path: '', component: ClienteComponent },
   { path: 'autor', component: AutorComponent },
   { path: 'categoria', component: CategoriaComponent },
+  { path: 'libro', component: LibroComponent },
 ];
 
 @NgModule({
