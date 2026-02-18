76edc88a9b3a45548e547400460be08a8d6f6c31
XavierT-157 <xtorres@tecnologicoismac.edu.ec>
2026-02-18 18:36:34 -0500

    feat: add ImagenController

--- ismac-librerianocturno20252026/src/main/java/com/distribuida/config/WebConfig.java
+++ ismac-librerianocturno20252026/src/main/java/com/distribuida/config/WebConfig.java
@@ -3,10 +3,11 @@ package com.distribuida.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public WebMvcConfigurer corsConfigurer(){
@@ -20,4 +21,11 @@ public class WebConfig {
             }
         };
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry){
+        resourceHandlerRegistry.addResourceHandler("/portadas/**")
+                .addResourceLocations("file:uploads/portadas/");
+    }
+
 }
--- ismac-librerianocturno20252026/src/main/java/com/distribuida/controller/ImagenController.java
+++ ismac-librerianocturno20252026/src/main/java/com/distribuida/controller/ImagenController.java
@@ -1,4 +1,28 @@
 package com.distribuida.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class ImagenController {
+
+    private static final String UPLOAD_DIR = "uploads/portadas/";
+
+    public ResponseEntity<Map<String, String>> uploadPortada(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam(value = "oldImage", required = false) String oldImage
+    ){
+        try{
+
+        }catch (Exception e){
+
+        }
+        return null;
+    }
 }
